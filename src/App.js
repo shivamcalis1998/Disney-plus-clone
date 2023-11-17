7500c5be8499ef29046e87f09e2cd6bb2d811c32
unknown <shivamswami8899@gmail.com>
2023-11-17 23:07:10 +0530

    till login done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
-
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Login />}></Route>
+          <Route exact path="/home" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,72 +8,118 @@ import movieIcon from "../images/movie-icon.svg";
 import seriesIcon from "../images/series-icon.svg";
 import { auth, provider } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import {
   selectUserName,
-  selectUserEmail,
   selectUserPhoto,
+  setUserLoginDetails,
 } from "../features/user/userSlice";
 
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const username = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        navigate("/home");
+      }
+    });
+  }, [username]);
+
   const handleAuth = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        setUser(result.user);
       })
       .catch((error) => {
         alert(error.message);
       });
   };
+
+  const setUser = (user) => {
+    dispatch(
+      setUserLoginDetails({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+      })
+    );
+  };
+
   return (
     <nav className={styles.header}>
       <a href="/" className={styles.headerLogo}>
         <img src={logo} className={styles.headerLogoImage} alt="Disney+" />
       </a>
-
-      <div className={styles.navMenu}>
-        <a href="/home" className={styles.navMenuLink}>
-          <img src={homeIcon} className={styles.navMenuImages} alt="HOME" />
-          <span>HOME</span>
+      {!username ? (
+        <a onClick={handleAuth} className={styles.login}>
+          login
         </a>
+      ) : (
+        <>
+          <div className={styles.navMenu}>
+            <a href="/home" className={styles.navMenuLink}>
+              <img src={homeIcon} className={styles.navMenuImages} alt="HOME" />
+              <span>HOME</span>
+            </a>
 
-        <a href="/" className={styles.navMenuLink}>
-          <img src={searchIcon} className={styles.navMenuImages} alt="SEARCH" />
-          <span>SEARCH</span>
-        </a>
+            <a className={styles.navMenuLink}>
+              <img
+                src={searchIcon}
+                className={styles.navMenuImages}
+                alt="SEARCH"
+              />
+              <span>SEARCH</span>
+            </a>
 
-        <a href="/" className={styles.navMenuLink}>
-          <img
-            src={watchlistIcon}
-            className={styles.navMenuImages}
-            alt="WATCHLIST"
-          />
-          <span>WATCHLIST</span>
-        </a>
+            <a className={styles.navMenuLink}>
+              <img
+                src={watchlistIcon}
+                className={styles.navMenuImages}
+                alt="WATCHLIST"
+              />
+              <span>WATCHLIST</span>
+            </a>
 
-        <a href="/" className={styles.navMenuLink}>
-          <img
-            src={orignalIcon}
-            className={styles.navMenuImages}
-            alt="ORIGNALS"
-          />
-          <span>ORIGNALS</span>
-        </a>
+            <a className={styles.navMenuLink}>
+              <img
+                src={orignalIcon}
+                className={styles.navMenuImages}
+                alt="ORIGNALS"
+              />
+              <span>ORIGNALS</span>
+            </a>
 
-        <a href="/" className={styles.navMenuLink}>
-          <img src={movieIcon} className={styles.navMenuImages} alt="MOVIES" />
-          <span>MOVIES</span>
-        </a>
+            <a className={styles.navMenuLink}>
+              <img
+                src={movieIcon}
+                className={styles.navMenuImages}
+                alt="MOVIES"
+              />
+              <span>MOVIES</span>
+            </a>
 
-        <a href="/" className={styles.navMenuLink}>
-          <img src={seriesIcon} className={styles.navMenuImages} alt="SERIES" />
-          <span>SERIES</span>
-        </a>
-      </div>
-      <a onClick={handleAuth} className={styles.login}>
+            <a className={styles.navMenuLink}>
+              <img
+                src={seriesIcon}
+                className={styles.navMenuImages}
+                alt="SERIES"
+              />
+              <span>SERIES</span>
+            </a>
+          </div>
+          <img className={styles.userImg} src={userPhoto} alt={username} />
+        </>
+      )}
+      {/* <a onClick={handleAuth} className={styles.login}>
         login
-      </a>
+      </a> */}
     </nav>
   );
 };
